29ea0f4707387aec05f97e4e97672854a839cb47
outlawstar4761 <outlawstar4761@gmail.com>
2025-04-12 08:56:57 -0500

    exposing new models

--- modelFactory.js
+++ modelFactory.js
@@ -1,14 +1,20 @@
 const Job = require('./models/job');
 const Execution = require('./models/execution');
+const Event = require('./models/event');
+const Subscription = require('./models/subscription');
 
 const models = {
   job: (id) => new Job(id),
-  execution: (id) => new Execution(id)
+  execution: (id) => new Execution(id),
+  event: (id) => new Event(id),
+  subscription: (id) => new Subscription(id),
 }
 
 const modelClasses = {
   job:Job,
-  execution:Execution
+  execution:Execution,
+  event:Event,
+  subscription:Subscription
 }
 
 module.exports = {
@@ -19,7 +25,7 @@ module.exports = {
     return models[name](id);
   },
   getClass: (name) => {
-    if(!models[name]){
+    if(!modelClasses[name]){
       throw new Error(`Model ${name} does not exist`);
     }
     return modelClasses[name];
